1967a3aa5657f80204c2f6f33da5ab6664656613
Cheikhibra10 <100207540+Cheikhibra10@users.noreply.github.com>
2024-03-01 01:24:44 +0000

    Update ICalcuIImple.java

--- src/implement/ICalcuIImple.java
+++ src/implement/ICalcuIImple.java
@@ -5,7 +5,7 @@ public class ICalcuIImple implements ICalcul {
     @Override
     public int add(int... x) throws Exception {
         if (x.length == 0){
-            throw new Exception("Veillez saisir au moins un entier ");
+            throw new Exception("Veillez saisir au moins un entier: ");
         }
         int somme = 0;
         for (int i = 0; i < x.length; i++) {
